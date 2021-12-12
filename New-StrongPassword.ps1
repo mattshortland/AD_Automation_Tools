e98083b4e73c236316848a4eb74cadcc3f11689c
mattshortland <matt.shortland@gmail.com>
2021-12-12 23:42:30 +0000

    Update New-StrongPassword.ps1
    set the passwordlength to not be required and use a sane option of 24 char as default password length. Fixed positional values to allow entry of length and non alpha chars without entering flags

--- New-StrongPassword.ps1
+++ New-StrongPassword.ps1
@@ -1,29 +1,59 @@
+<#
+Examples
+
+New-StrongPassword
+    generates a 24 character random password as plaintext
+New-StrongPassword -AsSecureString
+    generates a 24 character random password as a secure string
+New-StrongPassword -PasswordLength 15
+    generates a 15 character random password as plaintext
+New-StrongPassword 15
+    generates a 15 character random password as plaintext 
+New-StrongPassword -PasswordLength 8 -MinNonAlphaNumChars 1
+    generates an 8 character random password as plaintext, with at least 1 character not being alpha-numeric
+New-StrongPassword 8 1 -AsSecureString
+    generates an 8 character random password as plaintext, with at least 1 character not being alpha-numeric
+New-StrongPassword -PasswordLength 12 -AsSecureString
+    generates a 12 character random password as a secure string
+New-StrongPassword -PasswordLength 11 -AsObject
+    generates a 12 character random password as a PSCustomObject, with $_.text being the plaintext and $_.password being the secure string
+$userpassword = New-StrongPassword -PasswordLength 16 -AsObject
+Set-ADAccountPassword -Identity $username -NewPassword $userpassword.password
+write-host "User $username has the password $($userpassword.text)"
+    Here we show using the secure string property for the password change, and the plaintext property being used to output to the screen
+
+#>
+
 Function New-StrongPassword {
     [CmdletBinding(DefaultParameterSetName='AsText')]
 
 
 param (
-[Parameter(Mandatory=$true)][validatescript({<#Minimum 8 Characters for Passwords#> $_ -ge 8})][int]$PasswordLength,
-[Parameter (Mandatory=$false,Position=0,ParameterSetName='AsText')][switch]$AsText,
+[Parameter(Mandatory=$false,Position=0)][validatescript({<#Minimum 8 Characters for Passwords#> $_ -ge 8})][int]$PasswordLength=24,
+[Parameter (Mandatory=$false,Position=1)][validatescript({<#Minimum 1 Non AlphaNumeric Character for Passwords#> $_ -ge 1})][int]$MinNonAlphaNumChars,
+[Parameter (Mandatory=$false,Position=2,ParameterSetName='AsText')][switch]$AsText,
 [Parameter (Mandatory=$false,ParameterSetName='AsSecureString')][switch]$AsSecureString,
 [Parameter (Mandatory=$false,ParameterSetName='AsObject')][switch]$AsObject
 )
 
-$NonAlphaNumChars = Get-Random -Minimum 1 -Maximum ($PasswordLength - 4)
+If ($MinNonAlphaNumChars -eq $null)
+    {$MinNonAlphaNumChars = (Get-Random -Minimum 1 -Maximum ($PasswordLength - 4))}
+If ($MinNonAlphaNumChars -ge $PasswordLength)
+    {$MinNonAlphaNumChars = ($PasswordLength - 4)}
 
 Add-Type -AssemblyName System.Web
-$PasswordCheck = $false
+$PassComplexCheck = $false
 do {
-$NewPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLength,$NonAlphaNumChars)
+$NewPassword=[System.Web.Security.Membership]::GeneratePassword($PasswordLength,$MinNonAlphaNumChars)
 If ( ($NewPassword -cmatch "[A-Z\p{Lu}\s]") `
 -and ($NewPassword -cmatch "[a-z\p{Ll}\s]") `
 -and ($NewPassword -match "[\d]") `
 -and ($NewPassword -match "[^\w]")
 )
 {
-$PasswordCheck=$True
+$PassComplexCheck=$True
 }
-} While ($PasswordCheck -eq $false)
+} While ($PassComplexCheck -eq $false)
 
 if ($AsSecureString -eq $True)
     { return (ConvertTo-SecureString $NewPassword -AsPlainText -Force) }
@@ -36,10 +66,3 @@ elseif ($AsObject -eq $true)
     }
 else { return ($NewPassword)}
 }
-
-New-StrongPassword -PasswordLength 15
-New-StrongPassword -PasswordLength 15 -AsSecureString
-New-StrongPassword -PasswordLength 15 -AsObject
-(New-StrongPassword -PasswordLength 15 -AsObject).text
-(New-StrongPassword -PasswordLength 15 -AsObject).password
-
